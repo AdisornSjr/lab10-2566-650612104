d95f250a838ad025efffa6686e76f32857c015dd
Fluke <adisorn.sjr@gmail.com>
2023-07-24 16:44:57 +0700

    add key

--- src/app/page.js
+++ src/app/page.js
@@ -72,7 +72,7 @@ export default function RandomUserPage() {
             imgUrl={user.imgUrl}
             address={user.address}
             email={user.email}
-            key={user.key}
+            key={user.email}
           />
         ))}
     </div>
